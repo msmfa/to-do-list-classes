9fef175434d0a0703cc466f1fb7b453ba31e9403
Michael Sydney Moore <michaelsydneymoore@hotmail.com>
2020-03-21 16:38:31 +0000

    Added Header and componantfolder

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 
+import Header from "./components/Header";
 /*
 
 */
@@ -45,6 +46,7 @@ class App extends Component {
   render() {
     return (
       <>
+        <Header />
         <div className="App"></div>
         <Todos
           todos={this.state.todos}
